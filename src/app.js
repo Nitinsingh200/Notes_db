2362a62c48ae8c71624afa49eb9e4ab8f0ec7788
Nitinsingh200 <nitinsinghna12345@gamil.com>
2026-02-25 18:52:42 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const noteModel = require("./model/note.model")
+
+
+
+  const app = express();
+  app.use(express.json());
+
+
+
+
+  module.exports = app;
+
+   
